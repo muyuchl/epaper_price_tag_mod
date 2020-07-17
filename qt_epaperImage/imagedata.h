a22c23326d77557d925d3e9df4fed500c0f1cdc9
muyuchl <muyuchl@gmail.com>
2020-07-17 13:01:47 +0800

    add qt application to generate image bin file

---
+++ qt_epaperImage/imagedata.h
@@ -0,0 +1,20 @@
+#ifndef IMAGEDATA_H
+#define IMAGEDATA_H
+
+
+class ImageData
+{
+public:
+    ImageData();
+};
+
+
+extern const unsigned char img1[];
+
+extern const unsigned char img2[];
+
+extern const unsigned char imgMy[];
+
+extern const unsigned char img3[];
+
+#endif // IMAGEDATA_H
