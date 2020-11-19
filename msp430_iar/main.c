c90398a0e767a6e0fe0af49b05f497a6d7f522cd
muyuchl <muyuchl@gmail.com>
2020-11-19 15:24:55 +0800

    indent lines

--- msp430_iar/main.c
+++ msp430_iar/main.c
@@ -31,11 +31,11 @@ uint8_t next_pic_index = 0;
 
 void delay_ms(int count)
 {
-  for (int i = 0; i < count; i++)
-  {
-    for (int j = 0; j < 160; j++)
-      _NOP();
-  }
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < 160; j++)
+            _NOP();
+    }
 }
 
 static void check_rf();
@@ -43,23 +43,23 @@ static void switch_pic();
 
 int main( void )
 {
-  // Stop watchdog timer to prevent time out reset
-  WDTCTL = WDTPW + WDTHOLD;
+    // Stop watchdog timer to prevent time out reset
+    WDTCTL = WDTPW + WDTHOLD;
 
-  /* Use Calibration values for 1MHz Clock DCO*/
-DCOCTL = 0;
-BCSCTL1 = CALBC1_1MHZ + DIVA_3;  // XTS is 0, ACLK divide 8
-DCOCTL = CALDCO_1MHZ;
+    /* Use Calibration values for 1MHz Clock DCO*/
+    DCOCTL = 0;
+    BCSCTL1 = CALBC1_1MHZ + DIVA_3;  // XTS is 0, ACLK divide 8
+    DCOCTL = CALDCO_1MHZ;
 
-BCSCTL3 = LFXT1S_2; // VLO 12KHz
+    BCSCTL3 = LFXT1S_2; // VLO 12KHz
 
-// alt name TACCTL0
-CCTL0 = CCIE;                             // CCR0 interrupt enabled
-  TACTL = TASSEL_1 + MC_1 + ID_3;           // ACLK/8, upmode  
-  // ACLK 12kHz/8 = 1500Hz,  TA0 clk = 1500/8 = 187.5
-  CCR0 =  11250;   // 1 minute per interrupt
+    // alt name TACCTL0
+    CCTL0 = CCIE;                             // CCR0 interrupt enabled
+    TACTL = TASSEL_1 + MC_1 + ID_3;           // ACLK/8, upmode
+    // ACLK 12kHz/8 = 1500Hz,  TA0 clk = 1500/8 = 187.5
+    CCR0 =  11250;   // 1 minute per interrupt
 
-/*
+    /*
 uart_init();
 
 uart_send_str("a7105 communication ");
@@ -69,65 +69,65 @@ uart_send_str(__TIME__);
 uart_send_str("\r\n");
 */
 
-delay_ms(40);
-epd_init();
+    delay_ms(40);
+    epd_init();
 
-// rf on off pin
-P2DIR |= BIT7;
+    // rf on off pin
+    P2DIR |= BIT7;
 
-rfon;
-delay_ms(400);
+    rfon;
+    delay_ms(400);
 
-a7105_init();
-a7105_toRxMode();
-flash_init();
+    a7105_init();
+    a7105_toRxMode();
+    flash_init();
 
- __bis_SR_register( GIE); //  interrupts enabled
-  
-// display_menu();
+    __bis_SR_register( GIE); //  interrupts enabled
 
-  while(1)
-  {
-  	if (low_power_state) {
-  		if (tick >= SWITCH_PIC_TICK_COUNT) {
-  			tick = 0; // reset to 0
-  			// switch picture
-                        //epdon;
-                        epd_init();
-                        delay_ms(100);
-  			switch_pic();
-  			 			
-  		}
-                
-                epd_sleep();
-                delay_ms(1000);
-                epdoff;
-  		LPM3;
+    // display_menu();
 
-  	} else {
-  		check_rf();		
+    while(1)
+    {
+        if (low_power_state) {
+            if (tick >= SWITCH_PIC_TICK_COUNT) {
+                tick = 0; // reset to 0
+                // switch picture
+                //epdon;
+                epd_init();
+                delay_ms(100);
+                switch_pic();
 
-  		if (tick >= INITIAL_IDLE_TICK_COUNT) {
-                      tick = 0;	// reset to 0
-                        epd_init();
-                        delay_ms(100);                        
-  			switch_pic();
-                        
-  			// enter low power state
-  			low_power_state = 1;
-  			
-                        
-                        // not necessary, we are turning off the chip
-                        a7105_sleep();
-                        // turn off rf
-                        rfoff;
+            }
 
-  		}
-  	}
-            
-  }
+            epd_sleep();
+            delay_ms(1000);
+            epdoff;
+            LPM3;
 
- // return 0;
+        } else {
+            check_rf();
+
+            if (tick >= INITIAL_IDLE_TICK_COUNT) {
+                tick = 0;	// reset to 0
+                epd_init();
+                delay_ms(100);
+                switch_pic();
+
+                // enter low power state
+                low_power_state = 1;
+
+
+                // not necessary, we are turning off the chip
+                a7105_sleep();
+                // turn off rf
+                rfoff;
+
+            }
+        }
+
+    }
+
+    // return 0;
 }
 
 
@@ -135,63 +135,63 @@ flash_init();
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A (void)
 {
-  tick++;
+    tick++;
 
-  if (low_power_state
-  	&& tick >= SWITCH_PIC_TICK_COUNT) {
-  		LPM3_EXIT;
-  }
-  // 
+    if (low_power_state
+            && tick >= SWITCH_PIC_TICK_COUNT) {
+        LPM3_EXIT;
+    }
+    //
 }
 
 static void check_rf()
 {
-if (a7105_gio2_low() ) {
+    if (a7105_gio2_low() ) {
 
-    	if (a7105_rx_crc_ok()) {
-    		check_and_handle_rf_command();
+        if (a7105_rx_crc_ok()) {
+            check_and_handle_rf_command();
 
-    		// reset to 0, so not entering low power mode
-    		tick = 0;
-    	} else {
-    		crcErrCount++;
-              
-    		delay_ms(100);
-    		// read to discard
-    		a7105_read_rxdata(buf, RF_FRAME_LEN);
-    		delay_ms(10);
-    		a7105_strobe_cmd(CMD_STBY);
-    		delay_ms(10);
-    	}
+            // reset to 0, so not entering low power mode
+            tick = 0;
+        } else {
+            crcErrCount++;
 
-    	// back to rx mode
-    	a7105_toRxMode();
-    } 
+            delay_ms(100);
+            // read to discard
+            a7105_read_rxdata(buf, RF_FRAME_LEN);
+            delay_ms(10);
+            a7105_strobe_cmd(CMD_STBY);
+            delay_ms(10);
+        }
+
+        // back to rx mode
+        a7105_toRxMode();
+    }
 
 }
 
 static void switch_pic()
 {
-	const uint16_t ONE_READ_SIZE = 16;
+    const uint16_t ONE_READ_SIZE = 16;
 
-	uint8_t sector_index = next_pic_index;
-	next_pic_index++;
-	next_pic_index = next_pic_index & 0x3F;	// [0,63]
-	
-	uint32_t addr = 4096 * sector_index;
+    uint8_t sector_index = next_pic_index;
+    next_pic_index++;
+    next_pic_index = next_pic_index & 0x3F;	// [0,63]
 
-uint8_t img_buf[16];
+    uint32_t addr = 4096 * sector_index;
 
-epd_pre_update();
+    uint8_t img_buf[16];
 
-for (int i = 0; i < 4000 / ONE_READ_SIZE; i++) {
-  flash_read_data(img_buf, addr + i * ONE_READ_SIZE, ONE_READ_SIZE);
-  epd_update_ram(img_buf, ONE_READ_SIZE);
-}
+    epd_pre_update();
+
+    for (int i = 0; i < 4000 / ONE_READ_SIZE; i++) {
+        flash_read_data(img_buf, addr + i * ONE_READ_SIZE, ONE_READ_SIZE);
+        epd_update_ram(img_buf, ONE_READ_SIZE);
+    }
 
 
- epd_post_update();
+    epd_post_update();
 
-delay_ms(10* 1000); 
+    delay_ms(10* 1000);
 
 }
